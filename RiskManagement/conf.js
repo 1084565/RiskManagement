abd56898f883639835217b203d54d92c50ce5226
Irina Kirilova <irina.kirilova@sap.com>
2021-07-28 11:35:00 +0200

    Update conf.js

--- RiskManagement/conf.js
+++ RiskManagement/conf.js
@@ -1,6 +1,7 @@
 exports.config = {
   profile: "integration",
   baseUrl: "https://2f05827ftrial-dev-riskmanagement-approuter.cfapps.eu10.hana.ondemand.com/app/risks/webapp/index.html#Shell-home",
+  specs: ["./*.spec.js"] 
   auth: {
     // provide credentials as params when running the test:
     // uiveri5 --params.user=<user> --params.pass=<pass>
